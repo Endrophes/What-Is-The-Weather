27434ba4da17ce1e94cf85c663dbcd0c2a362cfd
Endrophes <Endrophes@users.noreply.github.com>
2023-03-07 12:58:30 -0500

    Update weatherPokerServer.py
    * Fixed spelling
    * Comment out unused imports

--- server/weatherPokerServer.py
+++ server/weatherPokerServer.py
@@ -3,21 +3,23 @@
 import sys
 import requests
 import json
-import subprocess
-import os
 import asyncio
 import websockets
-import _thread
+
+## Threading
+# import _thread
+# import subprocess
+# import os
 
 latitude = ""
 longitude = ""
 infoData = ""
-forcastData = ""
+forecastData = ""
 ipData = ""
 
-# Need to call InfoLink first to get info needed for forcastLink
+# Need to call InfoLink first to get info needed for forecastLink
 InfoLink = 'https://api.weather.gov/points/{},{}' # {latitude},{longitude}
-forcastLink = 'https://api.weather.gov/gridpoints/{}/{},{}/forecast' # {office}/{grid X},{grid Y}
+forecastLink = 'https://api.weather.gov/gridpoints/{}/{},{}/forecast' # {office}/{grid X},{grid Y}
 ipInfoLink = 'http://ipinfo.io/json'
 
 def getLocationFromIp():
@@ -30,12 +32,12 @@ def getInfoData():
     resText = response.text
     return json.loads(resText)
 
-def getForcastData(infoData):
+def getForecastData(infoData):
     gridId = infoData["properties"]["gridId"]
     gridX  = infoData["properties"]["gridX"]
     gridY  = infoData["properties"]["gridY"]
 
-    response = requests.get(forcastLink.format(gridId, gridX, gridY))
+    response = requests.get(forecastLink.format(gridId, gridX, gridY))
     resText = response.text
     return json.loads(resText)
 
@@ -43,11 +45,11 @@ def getLocal(infoData):
     location = infoData["properties"]["relativeLocation"]["properties"]
     return location["city"] + ", " + location["state"]
 
-def getTemp(forcastData):
-    return str(forcastData["properties"]["periods"][0]["temperature"]) + "°F"
+def getTemp(forecastData):
+    return str(forecastData["properties"]["periods"][0]["temperature"]) + "°F"
 
-def getDetail(forcastData):
-    return str(forcastData["properties"]["periods"][0]["detailedForecast"]) 
+def getDetail(forecastData):
+    return str(forecastData["properties"]["periods"][0]["detailedForecast"]) 
 
 # These need to come from command line
 numArgs = len(sys.argv)
@@ -65,15 +67,15 @@ async def receiver(websocket, path):
     print(f"< {action}")
     infoData = getInfoData()
     # print("infoData:" + str(infoData))
-    forcastData = getForcastData(infoData)
-    # print("forcastData:" + str(forcastData))
+    forecastData = getForecastData(infoData)
+    # print("forecastData:" + str(forecastData))
 
     if action == "get-temp":
-        response = getTemp(forcastData)
+        response = getTemp(forecastData)
     elif action == "get-local":
         response = getLocal(infoData)
     elif action == "get-detail":
-        response = getDetail(forcastData)
+        response = getDetail(forecastData)
     elif action == "exit":
         response = "ok bye"
         websocket.close()
@@ -84,7 +86,7 @@ async def receiver(websocket, path):
 # def launchApp():
 #     dir = os.path.dirname(__file__)
 #     path = os.path.join(dir, 'WhatIsTheWeather.exe')
-#     path = "C:\Users\\alexa\Desktop\BA-VerisonSubmission\WhatsTheWeather-exe\WhatIsTheWeather.exe"
+#     path = "WhatIsTheWeather.exe" # Path to exe file
 #     print("path:" + path)
 #     subprocess.run([path], shell=False)
 
